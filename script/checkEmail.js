428c237872a90b696e1318d1e601d13fe9afd3ba
ErykSandecki <eryk787@wp.pl>
2018-03-01 14:26:01 +0100

    Add security for bad email

--- script/checkEmail.js
+++ script/checkEmail.js
@@ -2,14 +2,24 @@ email =
 {
     onClick : function ()
     {
-        this.textEmail = document.getElementById("getEmail").value;
-        if((this.textEmail.indexOf('@',1) === -1) || (this.textEmail.indexOf('.',1) === -1))
+        this.textEmail = document.getElementById("get-Email").value;
+        this.badText = document.getElementById('bad-email');
+        this.badText.style.color = "red";
+        this.badText.style.textAlign = "center";
+
+        if(this.textEmail === '')
         {
-            setCssGame();
+            this.badText.innerText = "Nie podałeś emaila";
+        }
+
+        else if((this.textEmail.indexOf('@',1) === -1) || (this.textEmail.indexOf('.',1) === -1))
+        {
+            this.badText.innerText = "Nie poprawny format";
         }
 
         else
         {
+            this.badText.innerText = "";
             setCssGame();
         }
     },
